5955b238855d1fc5cdf6863206f7f632d13967ae
Miad Abdi <50056491+miadabdi@users.noreply.github.com>
2020-10-05 23:17:28 +0330

    Exported config to its own file

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const defaultConfig = {
+    image: {
+        withCaption: "figure", // figure or img (figcaption will be used for caption)
+        figureClass: "fig-img",
+        imgClass: "img",
+    },
+};
+
+module.exports = defaultConfig;
