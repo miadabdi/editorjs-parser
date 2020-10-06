04657a6c1a7b2f6389d82698980228c13039e216
Michael Jones <2001michaeljones@gmail.com>
2020-10-07 01:59:18 +0330

    fixed bug of calling functions

--- src/utitlities.js
+++ src/utitlities.js
@@ -1,27 +1,34 @@
-exports.isObject = function(item) {
+const isObject = function(item) {
     return item && typeof item === "object" && !Array.isArray(item);
 };
 
-exports.mergeDeep = function(target, source) {
+const mergeDeep = function(target, source) {
     let output = Object.assign({}, target);
     if (isObject(target) && isObject(source)) {
         Object.keys(source).forEach((key) => {
             if (isObject(source[key])) {
-                if (!(key in target)) Object.assign(output, {
-                    [key]: source[key] });
+                if (!(key in target))
+                    Object.assign(output, {
+                        [key]: source[key],
+                    });
                 else output[key] = mergeDeep(target[key], source[key]);
             } else {
                 Object.assign(output, {
-                    [key]: source[key] });
+                    [key]: source[key],
+                });
             }
         });
     }
     return output;
 };
 
-exports.sanitizeHtml = function(markup) {
+const sanitizeHtml = function(markup) {
     markup = markup.replace(/&/g, "&amp;");
     markup = markup.replace(/</g, "&lt;");
     markup = markup.replace(/>/g, "&gt;");
     return markup;
 };
+
+exports.sanitizeHtml = sanitizeHtml;
+exports.isObject = isObject;
+exports.mergeDeep = mergeDeep;
