fce0b483005d0a1887a90d3ab5edf7589288532a
Michael Jones <2001michaeljones@gmail.com>
2020-10-15 22:17:20 +0330

    Support for older browsers added using babel and esm version of code added as well

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+
 export default {
     input: "./src/Parser.js",
     output: [{
@@ -6,9 +8,30 @@ export default {
             name: "edjsParser",
         },
         {
+            file: "./build/Parser.esm.js",
+            format: "esm",
+            name: "edjsParser",
+            plugins: [getBabelOutputPlugin({
+                exclude: /node_modules/,
+                presets: [
+                    "@babel/preset-env",
+                ],
+                babelrc: false,
+                //allowAllFormats: true
+            })]
+        },
+        {
             file: "./build/Parser.browser.js",
             format: "iife",
             name: "edjsParser",
+            plugins: [getBabelOutputPlugin({
+                exclude: /node_modules/,
+                presets: [
+                    "@babel/preset-env",
+                ],
+                babelrc: false,
+                allowAllFormats: true
+            })]
         },
-    ],
+    ]
 };
